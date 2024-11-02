eb4e0b679e15c8b080a6a6dc583c5c5172c1d048
ItsOxas <121440551+OMaksele@users.noreply.github.com>
2024-11-02 23:20:39 +0200

    Testing

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="206df14e1b6c482abe4620a51d172c50",
-                                               client_secret="206df14e1b6c482abe4620a51d172c50",
-                                               redirect_uri="http://example.com",
+                                               client_secret="d385007c9b7f450fa6138030ff02a83c",
+                                               redirect_uri="http://localhost:8888/callback",
                                                scope="user-library-read"))
+user_profile = sp.current_user()
+print("User ID:", user_profile['id'])
+print("Display Name:", user_profile['display_name'])
+print("Email:", user_profile.get('email', 'Not Available'))
+print("Profile Image URL:", user_profile['images'][0]['url'] if user_profile['images'] else "No Image")
